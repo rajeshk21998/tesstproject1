cebabd606cff3f2d42f7442d5d97a2dd61f9fc17
rajeshk21998 <rajeshkasiviswanathan31@gmail.com>
2025-01-03 09:22:16 +0530

    yes

--- tesstproject1/src/tesstproject1/Test.java
+++ tesstproject1/src/tesstproject1/Test.java
@@ -3,6 +3,6 @@ package tesstproject1;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My First Java Programs.");
+        System.out.println("My First Java Programs.yes");
     }
 }
