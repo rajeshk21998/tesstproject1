191e719bf9d709d009ad736d102b3e3bf4c0e029
rajeshk21998 <rajeshkasiviswanathan31@gmail.com>
2025-01-04 12:30:35 +0530

    Update Test.java

--- tesstproject1/src/tesstproject1/Test.java
+++ tesstproject1/src/tesstproject1/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("My First Java Programs.yes");
          System.out.println("My First Java Programs");
-          System.out.println();
+         
 
           
     }
